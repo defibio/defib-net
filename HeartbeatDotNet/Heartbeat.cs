1c00b9533cc395047fcf6bf80904a25782276393
Riekelt Brands <riekelturk@yahoo.nl>
2016-04-28 21:48:28 +0200

    Init

---
+++ HeartbeatDotNet/Heartbeat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heartbeat
+{
+    public class Heartbeat
+    {
+        /// <summary>
+        /// The key provided by defib.io which allows you to send heartbeats
+        /// </summary>
+        public string key
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// How often a heartbeat should be sent to defib.io in seconds
+        /// </summary>
+        public int interval
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// The heart that will send heatbeats to Defib.IO at the given intervals
+        /// </summary>
+        private Heart heart;
+
+        /// <summary>
+        /// Creates a new Heartbeat instance
+        /// </summary>
+        /// <param name="defibKey">The key provided by defib.io which allows you to send heartbeats</param>
+        /// <param name="defibInterval">How often a heartbeat should be sent to defib.io in seconds</param>
+        public Heartbeat(string defibKey, int defibInterval)
+        {
+            Reporter.initialize();
+            heart = new Heart(defibKey, defibInterval);
+        }
+    }
+}
---
+++ HeartbeatTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Heartbeat;
+
+namespace HeartbeatTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hearbeats: 0");
+            Console.WriteLine("exceptions: 0");
+            Console.WriteLine("failed: 0");
+
+            Heartbeat.Heartbeat heartbeat = new Heartbeat.Heartbeat("TEST", 30);
+
+            while (true)
+            {
+                Console.SetCursorPosition(11, 0);
+                Console.Write(Heartbeat.Reporter.beats);
+                Console.SetCursorPosition(12, 1);
+                Console.Write(Heartbeat.Reporter.exceptions);
+                Console.SetCursorPosition(8, 2);
+                Console.Write(Heartbeat.Reporter.errors);
+            }
+        }
+    }
+}
